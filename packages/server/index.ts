93c904aa132c534c6a1abd4a6009acc433a57c27
Vikas Choudhary <vikaspc29@gmail.com>
2025-08-30 23:01:07 +0530

    frontend and backend setup

---
+++ packages/server/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import type { Request, Response } from "@types/express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(`open aki key ${process.env.OPENAI_API_KEY}`);
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhosppt:${port}`);
+});
+
