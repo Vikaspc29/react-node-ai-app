3690ecf9ca7ff2522e52b51ae72b5084394ad8c1
Vikas Choudhary <vikaspc29@gmail.com>
2025-08-31 21:57:42 +0530

    Frotend, backend, pritter and husky setup

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,6 @@
-import express from "express";
-import type { Request, Response } from "@types/express";
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from '@types/express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -8,10 +8,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send(`open aki key ${process.env.OPENAI_API_KEY}`);
-})
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhosppt:${port}`);
+   res.send(`open aki key ${process.env.OPENAI_API_KEY}`);
 });
 
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'Hello World!' });
+});
+
+app.listen(port, () => {
+   console.log(`Server is running on http://localhosppt:${port}`);
+});
