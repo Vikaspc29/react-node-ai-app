2e6675c4a5b973d588358e2853b2f71be2676a9a
Vikas Choudhary <vikaspc29@gmail.com>
2025-09-13 10:57:08 +0530

    Added backend chat contoller and service

---
+++ packages/server/repositories/conversation.repository.ts
@@ -0,0 +1,13 @@
+// Implemetation detail
+const conversations = new Map<string, string>();
+
+// Export public interface
+export const conversationRepository = {
+   getLastResponseId(conversationId: string) {
+      return conversations.get(conversationId);
+   },
+
+   setLastResponseId(conversationId: string, responseId: string) {
+      return conversations.set(conversationId, responseId);
+   },
+};
